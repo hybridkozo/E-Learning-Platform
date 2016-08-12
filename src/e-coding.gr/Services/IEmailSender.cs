e84b15a532bc06ab9c116920a297422c6116fb65
hybridkozo <g.kozompolis@gmail.com>
2016-08-12 23:09:54 +0300

    New Project New Header more changes

---
+++ src/e-coding.gr/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_coding.gr.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
